d38f618b41d41b27ca9606ae6aac92a917854549
am999 <am9999072080@gmail.com>
2023-03-18 13:00:45 +0300

    Correction-1

--- src/Main.java
+++ src/Main.java
@@ -65,32 +65,44 @@ public class Main {
 
     public static void task5() {
         System.out.println("Задача-5");
-        int monthNumber = 12;
+        int monthNumber = 2;
         switch (monthNumber) {
             case 1:
                 System.out.println(monthNumber + "-й месяц принадлежит к сезону зима");
+                break;
             case 2:
                 System.out.println(monthNumber + "-й месяц принадлежит к сезону зима");
+                break;
             case 3:
                 System.out.println(monthNumber + "-й месяц принадлежит к сезону весна");
+                break;
             case 4:
                 System.out.println(monthNumber + "-й месяц принадлежит к сезону весна");
+                break;
             case 5:
                 System.out.println(monthNumber + "-й месяц принадлежит к сезону весна");
+                break;
             case 6:
                 System.out.println(monthNumber + "-й месяц принадлежит к сезону лето");
+                break;
             case 7:
                 System.out.println(monthNumber + "-й месяц принадлежит к сезону лето");
+                break;
             case 8:
                 System.out.println(monthNumber + "-й месяц принадлежит к сезону лето");
+                break;
             case 9:
                 System.out.println(monthNumber + "-й месяц принадлежит к сезону осень");
+                break;
             case 10:
                 System.out.println(monthNumber + "-й месяц принадлежит к сезону осень");
+                break;
             case 11:
                 System.out.println(monthNumber + "-й месяц принадлежит к сезону осень");
+                break;
             case 12:
                 System.out.println(monthNumber + "-й месяц принадлежит к сезону зима");
+
                 break;
             default:
                 if (monthNumber > 12) {
